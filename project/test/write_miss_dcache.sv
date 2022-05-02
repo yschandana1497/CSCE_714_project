4321cd128c134bd9ca91e6182af88baad934683f
Gnanesh789 <90413736+Gnanesh789@users.noreply.github.com>
2022-05-01 22:00:40 -0500

    Update write_miss_dcache.sv

--- project/test/write_miss_dcache.sv
+++ project/test/write_miss_dcache.sv
@@ -43,7 +43,33 @@ class write_miss_seq extends base_vseq;
     endfunction : new
 
     virtual task body();
+        /* Bug-10
+		`uvm_do_on_with(trans, p_sequencer.cpu_seqr[mp], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC;address == 32'hFEDC_CDEF;data == 32'hABCD_EF01;})
+		`uvm_do_on_with(trans, p_sequencer.cpu_seqr[sp1], {request_type == READ_REQ; access_cache_type == DCACHE_ACC;address == 32'hFEDC_CDEF;})
+		`uvm_do_on_with(trans, p_sequencer.cpu_seqr[sp1], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC;address == 32'hFEDC_CDEF;data == 32'hEF01_ABCD;})
+        */
+        /* Bug-9
+		`uvm_do_on_with(trans, p_sequencer.cpu_seqr[mp], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC;address == 32'hFEDC_CDEF;data == 32'hABCD_EF01;})
+		`uvm_do_on_with(trans, p_sequencer.cpu_seqr[sp1], {request_type == READ_REQ; access_cache_type == DCACHE_ACC;address == 32'hFEDC_CDEF;})
+		`uvm_do_on_with(trans, p_sequencer.cpu_seqr[sp1], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC;address == 32'hFEDC_CDEF;data == 32'hEF01_ABCD;})
+        */
+        /*Bug-8
+        `uvm_do_on_with(trans, p_sequencer.cpu_seqr[0], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC;address == 32'hF000_0000;data == 32'h0000_0000;})
+		`uvm_do_on_with(trans, p_sequencer.cpu_seqr[1], {request_type == READ_REQ; access_cache_type == DCACHE_ACC;address == 32'hF000_0000;})
         
+        `uvm_do_on_with(trans, p_sequencer.cpu_seqr[0], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC;address == 32'hFF00_0000;data == 32'1111_1111;})
+		`uvm_do_on_with(trans, p_sequencer.cpu_seqr[2], {request_type == READ_REQ; access_cache_type == DCACHE_ACC;address == 32'hFF00_0000;})
+        
+        `uvm_do_on_with(trans, p_sequencer.cpu_seqr[0], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC;address == 32'hFFF0_0000;data == 32'h2222_2222;})
+		`uvm_do_on_with(trans, p_sequencer.cpu_seqr[3], {request_type == READ_REQ; access_cache_type == DCACHE_ACC;address == 32'hFFF0_0000;})
+        
+        `uvm_do_on_with(trans, p_sequencer.cpu_seqr[0], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC;address == 32'hFFFF_0000;data == 32'h3333_3333;})
+		`uvm_do_on_with(trans, p_sequencer.cpu_seqr[1], {request_type == READ_REQ; access_cache_type == DCACHE_ACC;address == 32'hFFFF_0000;})
+		`uvm_do_on_with(trans, p_sequencer.cpu_seqr[0], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC;address == 32'hFEDC_CDEF;data == 32'h6000_0000;})
+        */
+        /* Bug-3
+        `uvm_do_on_with(trans, p_sequencer.cpu_seqr[mp], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC;address == 32'hFEDC_CDEF;data == 32'hABCD_EF01;})
+        */
         `uvm_do_on_with(trans, p_sequencer.cpu_seqr[0], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC; })
         trans_addr = trans.address;
 
