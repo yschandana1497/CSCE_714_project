5448a7c41801581380143612c4a5d72cdc004794
bhaveshsharma71 <69723495+bhaveshsharma71@users.noreply.github.com>
2022-05-01 22:16:58 -0500

    Update read_hit_dcache.sv
    qwesad

--- project/test/read_hit_dcache.sv
+++ project/test/read_hit_dcache.sv
@@ -17,7 +17,7 @@ class read_hit_dcache extends base_test;
 
     //UVM build phase
     function void build_phase(uvm_phase phase);
-        uvm_config_wrapper::set(this, "tb.vsequencer.run_phase", "default_sequence", read_hit_dcache_seq::type_id::get());
+        uvm_config_wrapper::set(this, "tb.vsequencer.run_phase", "default_sequence", read_hit_dcache_test::type_id::get());
         super.build_phase(phase);
     endfunction : build_phase
 
@@ -30,18 +30,18 @@ endclass : read_hit_dcache
 
 
 // Sequence for a read-miss on D-cache
-class read_hit_dcache_seq extends base_vseq;
+class read_hit_dcache_test extends base_vseq;
     //object macro
-    `uvm_object_utils(read_hit_dcache_seq)
+    `uvm_object_utils(read_hit_dcache_test)
 
     cpu_transaction_c trans;
-    int i,j;
+    int i,j,k;
     bit [`ADDR_WID_LV1 - 1 : 0] trans_addr;
 
-    random_dcache_address random_address_handle = new(); // Vaibhavi:  class in cpu_transaction_c.sv file
+    random_dcache_address random_address_handle = new();
 
     //constructor
-    function new (string name="read_hit_dcache_seq");
+    function new (string name="read_hit_dcache_test");
         super.new(name);
     endfunction : new
 
@@ -49,27 +49,24 @@ class read_hit_dcache_seq extends base_vseq;
     assert( random_address_handle.randomize());
     random_address_handle.randomize_address();
 
-    /***********************************************************************************************************************************************/
-    // Read hit to all cpu dcache
-    
-    for(int i =0; i<4; i++) begin
+    	i=0;
+ 	repeat(4) begin
         `uvm_do_on_with(trans, p_sequencer.cpu_seqr[i], {request_type == READ_REQ; access_cache_type == DCACHE_ACC; address inside {random_address_handle.random_addr};})
          trans_addr = trans.address;
-        `uvm_do_on_with(trans, p_sequencer.cpu_seqr[i], {request_type == READ_REQ; access_cache_type == DCACHE_ACC; address == trans_addr;})  
+        `uvm_do_on_with(trans, p_sequencer.cpu_seqr[i], {request_type == READ_REQ; access_cache_type == DCACHE_ACC; address == trans_addr;}) 
+	   i=i+1; 
     end
-    
-    /***********************************************************************************************************************************************/
-    // Read hit on complete set  4 writes 4 reads
-    
-    for ( j =0; j<4; j++)begin
-    for( i =0; i<4; i++)begin
-        `uvm_do_on_with(trans, p_sequencer.cpu_seqr[j], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC; address == random_address_handle.random_addr[i]; data == i;}) //random data
-    end
-    for( i =0; i<4; i++)begin
-      `uvm_do_on_with(trans, p_sequencer.cpu_seqr[j], {request_type == READ_REQ; access_cache_type == DCACHE_ACC; address == random_address_handle.random_addr[i];})
-    end
+    j=0;
+repeat(4)begin
+	k=0;
+   	 repeat(4)begin
+     	   `uvm_do_on_with(trans, p_sequencer.cpu_seqr[j], {request_type == WRITE_REQ; access_cache_type == DCACHE_ACC; address == random_address_handle.random_addr[k]; data == k;}) //random data
+     	   `uvm_do_on_with(trans, p_sequencer.cpu_seqr[j], {request_type == READ_REQ; access_cache_type == DCACHE_ACC; address == random_address_handle.random_addr[k];})
+	    k=k+1;
+   	 end
+ 	j=j+1;
     end
     
     endtask
 
-endclass : read_hit_dcache_seq
+endclass : read_hit_dcache_test
